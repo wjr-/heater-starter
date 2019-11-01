c3cc0c2048ac6d96d9ad6c8ff50748ec5fc22707
Ville Ruuskanen <ville.ruuskanen@arado.fi>
2019-11-01 13:20:34 +0200

    Switched message sending library from sms to flutter_sms.
    Old library used an obsolete package.

--- heater_starter/lib/heater_control.dart
+++ heater_starter/lib/heater_control.dart
@@ -1,16 +1,19 @@
 import "dart:async";
 
-import 'package:sms/sms.dart';
+import 'package:flutter_sms/flutter_sms.dart';
 
 import 'heater_control_messages.dart';
 
 class HeaterControl {
   Future<dynamic> start(String phoneNumber, String pin, int minutes) async {
     var startMessage = new StartMessage(pin: pin, duration: minutes);
-    SmsSender sender = new SmsSender();
 
-    return await sender
-        .sendSms(new SmsMessage(phoneNumber, startMessage.toString()));
-    // handle errors?
+    return await _sendSMS(startMessage.toString(), phoneNumber);
+  }
+
+  Future _sendSMS(String message, String recipient) async {
+    var recipients = [recipient];
+
+    return await FlutterSms.sendSMS(message: message, recipients: recipients);
   }
 }
